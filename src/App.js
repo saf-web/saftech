fbfe8eb4555fb832d63559324bc691c222d8289f
unknown <saf-web>
2024-04-18 00:14:15 +0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/forgot-password" element={< ForgotPassword/>} />
+      </Routes>
+    </Router>
   );
 }
 
